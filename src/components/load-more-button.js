720a18f7a36e9d106698f65ae5a94433e834d694
PavelLikharev <pavel7321@mail.ru>
2020-05-04 23:14:28 +0700

    Выполнил ДЗ 5

--- src/components/load-more-button.js
+++ src/components/load-more-button.js
@@ -1,27 +1,15 @@
-import {createElement} from 'src/utils.js';
+import AbstractComponent from 'src/components/abstract-component.js';
 
 const createLoadMoreButtonTemplate = () => {
   return `<button class="load-more" type="button">load more</button>`;
 };
 
-export default class LoadMoreButton {
-  constructor() {
-    this._element = null;
-  }
-
+export default class LoadMoreButton extends AbstractComponent {
   getTemplate() {
     return createLoadMoreButtonTemplate();
   }
 
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
+  setClickHandler(handler) {
+    this.getElement().addEventListener(`click`, handler);
   }
 }
---
+++ src/components/no-tasks.js
@@ -0,0 +1,15 @@
+import AbstractElement from 'src/components/abstract-component.js';
+
+const createNoTasksTemplate = () => {
+  return (
+    `<p class="board__no-tasks">
+      Click «ADD NEW TASK» in menu to create your first task
+    </p>`
+  );
+};
+
+export default class NoTasks extends AbstractElement {
+  getTemplate() {
+    return createNoTasksTemplate();
+  }
+}
