cfca73d298b04b5bcbf8abc37fb6244fec4278b7
PavelLikharev <pavel7321@mail.ru>
2020-05-30 20:47:44 +0700

    Добавлена вкладка статистики и функционал её показа

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import BoardComponent from "src/components/board.js";
 import BoardController from 'src/controllers/board.js';
 import FilterController from "src/controllers/filter.js";
+import StatisticsComponent from "src/components/statistics.js";
 import SiteMenuComponent, {MenuItem} from "./components/site-menu.js";
 import TasksModel from "src/models/tasks.js";
 import {generateTasks} from "src/mock/task.js";
@@ -27,11 +28,31 @@ render(siteMainElement, boardComponent, RenderPosition.BEFOREEND);
 const boardController = new BoardController(boardComponent, tasksModel);
 boardController.render();
 
+const dateTo = new Date();
+const dateFrom = (() => {
+  const d = new Date(dateTo);
+  d.setDate(d.getDate() - 7);
+  return d;
+})();
+const statisticsComponent = new StatisticsComponent({tasks: tasksModel, dateFrom, dateTo});
+render(siteMainElement, statisticsComponent, RenderPosition.BEFOREEND);
+statisticsComponent.hide();
+
 siteMenuComponent.setOnChange((menuItem) => {
   switch (menuItem) {
     case MenuItem.NEW_TASK:
       siteMenuComponent.setActiveItem(MenuItem.TASKS);
+      statisticsComponent.hide();
+      boardController.show();
       boardController.createTask();
+      break;
+    case MenuItem.STATISTICS:
+      boardController.hide();
+      statisticsComponent.show();
+      break;
+    case MenuItem.TASKS:
+      statisticsComponent.hide();
+      boardController.show();
       break;
   }
 });
